38db8e5e0d05e5dbfa8846ffaf534a9fc8300dcd
moqi <moqimoqidea@gmail.com>
2020-09-24 21:42:14 +0800

    Add Greetings

---
+++ src/main/groovy/com/moqi/Greetings.groovy
@@ -0,0 +1,16 @@
+package com.moqi
+
+/**
+ * @author moqi* On 9/24/20 21:40
+ */
+public class Greetings {
+
+    public static void main(String[] args) {
+        for (int i = 0; i < 3; i++) {
+            System.out.print("ho ")
+        }
+
+        System.out.println("Merry Groovy!")
+    }
+
+}
