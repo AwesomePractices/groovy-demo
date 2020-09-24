01748dd8c1cfe2b129c43c3be0a8ef9a82572498
moqi <moqimoqidea@gmail.com>
2020-09-24 22:36:55 +0800

    Add 链式调用

---
+++ src/main/groovy/com/moqi/A06ChainCall.groovy
@@ -0,0 +1,29 @@
+package com.moqi
+
+/**
+ * @author moqi* On 9/24/20 22:33
+ */
+class A06ChainCall {
+
+    static void main(String[] args) {
+        Wizard.learn('alohomora', {/*...*/ })
+                .learn('expelliarmus', {/*...*/ })
+                .learn('lumos', {/*...*/ })
+    }
+
+}
+
+/**
+ * 静态方法内可以使用 this 来引用 Class 对象
+ */
+class Wizard {
+
+    /**
+     * learn 返回的是 Class 对象，所以可以使用链式调用
+     */
+    def static learn(trick, action) {
+        // ...
+        this
+    }
+
+}
