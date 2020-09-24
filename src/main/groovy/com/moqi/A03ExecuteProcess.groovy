25f34277004aa49bc42341ef3669d9543335c1e9
moqi <moqimoqidea@gmail.com>
2020-09-24 22:03:51 +0800

    Add Execute Process

---
+++ src/main/groovy/com/moqi/A03ExecuteProcess.groovy
@@ -0,0 +1,12 @@
+package com.moqi
+
+/**
+ * @author moqi* On 9/24/20 22:02
+ */
+class A03ExecuteProcess {
+
+    static void main(String[] args) {
+        println "svn help".execute().text
+    }
+
+}
---
+++ src/main/java/com/moqi/A03JavaExecuteProcess.java
@@ -0,0 +1,27 @@
+package com.moqi;
+
+import java.io.*;
+import java.util.Objects;
+
+/**
+ * @author moqi
+ * On 9/24/20 21:59
+ */
+
+public class A03JavaExecuteProcess {
+
+    public static void main(String[] args) {
+        try {
+            Process proc = Runtime.getRuntime().exec("svn help");
+            BufferedReader result = new BufferedReader(new InputStreamReader(proc.getInputStream()));
+            String line;
+
+            while (Objects.nonNull((line = result.readLine()))) {
+                System.out.println("line = " + line);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
