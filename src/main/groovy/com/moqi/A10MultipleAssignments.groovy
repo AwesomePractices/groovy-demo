6dea52578a629eff00184192f90a6a7db5f9ad95
moqi <moqimoqidea@gmail.com>
2020-09-26 21:07:00 +0800

    使用多赋值

---
+++ src/main/groovy/com/moqi/A10MultipleAssignments.groovy
@@ -0,0 +1,24 @@
+package com.moqi
+
+/**
+ * 使用多赋值
+ *
+ * @author moqi* On 9/26/20 21:02
+ */
+static def splitName(fullName) { fullName.split(' ') }
+
+def (firstName, lastName) = splitName('James Bond')
+println("$lastName, $firstName $lastName")
+
+def name1 = 'tom'
+def name2 = 'jack'
+
+println("$name1 ans $name2")
+(name1, name2) = [name2, name1]
+println("$name1 ans $name2")
+
+def (String cat, String mouse) = ['Tom', 'Jerry', 'Spike', 'Tyke']
+println("$cat and $mouse")
+
+def (first, second, third) = ['Tom', 'Jerry']
+println("$first, $second, $third")
