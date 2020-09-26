51386839517b4961ee528102c502530f04051515
moqi <moqimoqidea@gmail.com>
2020-09-26 21:01:31 +0800

    可选形参

---
+++ src/main/groovy/com/moqi/A09OptionalParameters.groovy
@@ -0,0 +1,22 @@
+package com.moqi
+
+/**
+ * 可选形参
+ *
+ * @author moqi* On 9/26/20 20:58
+ */
+static def log(x, base=10) {
+    Math.log(x) / Math.log(base)
+}
+
+println(log(1024))
+println(log(1024, 10))
+println(log(2014, 2))
+
+static def task(name, String[] details) {
+    println("$name - $details")
+}
+
+task 'Call', '123-456-7890'
+task 'Call', '123-456-7890', '231-546-0987'
+task 'Check Mail'
