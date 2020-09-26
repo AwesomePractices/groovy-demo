b69e8a41a94b5b9f5c572478142b555acb0d413c
moqi <moqimoqidea@gmail.com>
2020-09-26 21:18:55 +0800

    实现接口

---
+++ src/main/groovy/com/moqi/A11Swing.groovy
@@ -0,0 +1,44 @@
+package com.moqi
+
+import javax.swing.*
+import java.awt.*
+import java.awt.event.*
+
+/**
+ * 实现接口
+ *
+ * @author moqi* On 9/26/20 21:17
+ */
+frame = new JFrame(size: [300, 300],
+        layout: new FlowLayout(),
+        defaultCloseOperation: WindowConstants.EXIT_ON_CLOSE)
+button = new JButton("click")
+positionLabel = new JLabel("")
+msgLabel = new JLabel("")
+frame.contentPane.add button
+frame.contentPane.add positionLabel
+frame.contentPane.add msgLabel
+
+button.addActionListener(
+        { JOptionPane.showMessageDialog(frame, "You clicked!") } as ActionListener
+)
+
+displayMouseLocation = { positionLabel.setText("$it.x, $it.y") }
+frame.addMouseListener(displayMouseLocation as MouseListener)
+frame.addMouseMotionListener(displayMouseLocation as MouseMotionListener)
+
+handleFocus = [
+        focusGained: { msgLabel.setText("Good to see you!") },
+        focusLost  : { msgLabel.setText("Come back soon!") }
+]
+button.addFocusListener(handleFocus as FocusListener)
+
+events = ['WindowListener', 'ComponentListener']
+// Above list may be dynamic and may come from some input
+handler = { msgLabel.setText("$it") }
+for (event in events) {
+    handlerImpl = handler.asType(Class.forName("java.awt.event.${event}"))
+    frame."add${event}"(handlerImpl)
+}
+
+frame.show()
