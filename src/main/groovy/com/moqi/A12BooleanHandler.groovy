1a1e85aecbfe29753005f711553246a11c83b2c8
moqi <moqimoqidea@gmail.com>
2020-09-26 21:22:58 +0800

    布尔值判断

---
+++ src/main/groovy/com/moqi/A12BooleanHandler.groovy
@@ -0,0 +1,18 @@
+package com.moqi
+
+/**
+ * 布尔值判断
+ *
+ * @author moqi* On 9/26/20 21:20
+ */
+str = 'hello'
+if (str) {
+    println("It's True")
+}
+
+lst0 = null
+print lst0 ? 'lst0 true' : 'lst0 false'
+lst1 = [1, 2, 3]
+print lst1 ? 'lst1 true' : 'lst1 false'
+lst2 = []
+print lst2 ? 'lst2 true' : 'lst2 false'
