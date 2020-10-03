4303ed4b80f6bb15b5788e563107bd71452d9634
moqi <moqimoqidea@gmail.com>
2020-10-03 15:49:37 +0800

    闭包与协程

---
+++ src/main/groovy/com/moqi/ch04/A08Coroutine.groovy
@@ -0,0 +1,22 @@
+package com.moqi.ch04
+
+/**
+ * 闭包与协程
+ *
+ * @author moqi* On 10/3/20 15:46
+ */
+def iterate(n, closure) {
+    1.upto(n) {
+        println("In iterate with value ${it}")
+        closure(it)
+    }
+}
+
+println("Calling iterate")
+total = 0
+iterate(4) {
+    total += it
+    println("In closure total so far is ${total}")
+}
+println("Done")
+
