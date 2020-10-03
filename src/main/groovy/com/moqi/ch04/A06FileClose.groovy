710acc1f13e925e8d1f03dbc75396876083ab5d7
moqi <moqimoqidea@gmail.com>
2020-10-03 15:22:23 +0800

    使用闭包进行资源清理 01

---
+++ src/main/groovy/com/moqi/ch04/A06FileClose.groovy
@@ -0,0 +1,16 @@
+package com.moqi.ch04
+
+/**
+ * 使用闭包进行资源清理 01
+ *
+ * @author moqi* On 10/3/20 15:06
+ */
+def outputTxtFile = '/Users/moqi/Downloads/output.txt'
+writer = new FileWriter(outputTxtFile)
+writer.write('!')
+// 忘记调用 writer.close()
+
+
+new FileWriter(outputTxtFile).withWriter { writer ->
+    writer.write('!!!')
+} // 不再需要自己调用 close()
