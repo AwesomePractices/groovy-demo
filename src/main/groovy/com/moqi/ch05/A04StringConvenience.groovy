7c67a0d950207758ef3d996863881e5017dcf7b8
moqi <moqimoqidea@gmail.com>
2020-10-05 10:36:14 +0800

    字符串便捷方法

---
+++ src/main/groovy/com/moqi/ch05/A04StringConvenience.groovy
@@ -0,0 +1,18 @@
+package com.moqi.ch05
+
+/**
+ * 字符串便捷方法
+ *
+ * @author moqi On 10/5/20 10:31
+ */
+str1 = "It's a rainy day in seattle"
+println(str1)
+
+str1 -= 'rainy '
+println(str1)
+println()
+
+for (str in 'held'..'helm') {
+    print("$str ")
+}
+println()
