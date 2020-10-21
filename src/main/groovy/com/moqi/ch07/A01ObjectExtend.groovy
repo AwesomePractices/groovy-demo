b1e2219c2919a265fc0dd60cfb2354d27c2c8d86
moqi <moqimoqidea@gmail.com>
2020-10-21 16:02:58 +0800

    Object 类的扩展 + 1

--- src/main/groovy/com/moqi/ch07/A01ObjectExtend.groovy
+++ src/main/groovy/com/moqi/ch07/A01ObjectExtend.groovy
@@ -11,3 +11,31 @@ println(str)
 println(str.dump())
 println()
 
+lst = [1, 2]
+lst.add(3)
+lst.add(4)
+println(lst.size())
+println(lst.contains(2))
+println()
+
+lst1 = [1, 2]
+lst1.with {
+    add(3)
+    add(4)
+    println(size())
+    println(contains(2))
+}
+println()
+
+lst1.with {
+    println("this is ${this}")
+    println("owner is ${owner}")
+    println("delegate is ${delegate}")
+}
+println()
+
+
+
+
+
+
