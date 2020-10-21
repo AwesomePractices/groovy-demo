db65ccbd7cea3799c5ab1d3830abdeb5fb7fffcb
moqi <moqimoqidea@gmail.com>
2020-10-21 16:12:49 +0800

    Object 类的扩展 + 2

--- src/main/groovy/com/moqi/ch07/A01ObjectExtend.groovy
+++ src/main/groovy/com/moqi/ch07/A01ObjectExtend.groovy
@@ -34,8 +34,38 @@ lst1.with {
 }
 println()
 
+thread = Thread.start {
+    println("Thread started")
+    startTime = System.nanoTime()
+    new Object().sleep(2000)
+    endTime = System.nanoTime()
+    println("Thread done in ${(endTime - startTime) / 10 ** 9} seconds")
+}
+new Object().sleep(100)
+println("Let's interrupt that thread")
+thread.interrupt()
+thread.join()
+println()
 
+def playWithSleep(flag) {
+    thread = Thread.start {
+    println("Thread started")
+    startTime = System.nanoTime()
+    new Object().sleep(2000) {
+        println("Interrupted ... " + it)
+        flag
+    }
+    endTime = System.nanoTime()
+    println("Thread done in ${(endTime - startTime) / 10 ** 9} seconds")
+    }
 
+    thread.interrupt()
+    thread.join()
+    println()
+}
+
+playWithSleep(true)
+playWithSleep(false)
 
 
 
