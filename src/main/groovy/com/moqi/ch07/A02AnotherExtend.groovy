3503977f751e354f7956a8a555fe109392133f16
moqi <moqimoqidea@gmail.com>
2020-10-22 10:11:34 +0800

    其他扩展 + 使用 java.lang 的扩展

--- src/main/groovy/com/moqi/ch07/A02AnotherExtend.groovy
+++ src/main/groovy/com/moqi/ch07/A02AnotherExtend.groovy
@@ -9,9 +9,46 @@ int[] arr1 = [1, 2, 3, 4, 5, 6]
 println(arr1[2..4])
 println()
 
+process1 = "wc".execute()
+process1.out.withWriter {
+    // 将输入发送到进程
+    it << "Let the world know...\n"
+    it << "Groovy Rocks!\n"
+}
+println(process1.in.text)
+println()
 
+// fixme command1.execute().text 未打印出任何东西
+String[] command1 = ['groovy', '-e', '"print \'Groovy\'"']
+println("Calling ${command1.join(' ')}")
+println(command1.execute().text)
+println()
 
+def printThreadInfo(msg) {
+    def currentThread = Thread.currentThread()
+    println("$msg Thread is ${currentThread}: Daemon? ${currentThread.isDaemon()}")
+}
+printThreadInfo('Main')
 
+Thread.start {
+    printThreadInfo("Started")
+    sleep(3000) {
+        println("Interrupted")
+    }
+    println("Finished Started")
+}
+
+sleep(1000)
+
+Thread.startDaemon {
+    printThreadInfo("Started Daemon")
+    sleep(5000) {
+        println("Interrupted")
+    }
+    // 不会执行到这里
+    println("Finished Started Daemon")
+}
+println()
 
 
 
