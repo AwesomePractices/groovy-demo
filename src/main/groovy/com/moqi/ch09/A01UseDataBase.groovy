4e5a778d9f01da4684a55ee79ec0a5b983531450
moqi <moqimoqidea@gmail.com>
2020-10-23 10:29:12 +0800

    使用数据库 完成

--- src/main/groovy/com/moqi/ch09/A01UseDataBase.groovy
+++ src/main/groovy/com/moqi/ch09/A01UseDataBase.groovy
@@ -1,6 +1,7 @@
 package com.moqi.ch09
 
 import groovy.sql.Sql
+import groovy.xml.MarkupBuilder
 
 /**
  * 使用数据库
@@ -23,8 +24,8 @@ sql.eachRow('select * from weather') {
 }
 println()
 
-ProcessMeta = {metaData ->
-    metaData.columnCount.times {i ->
+ProcessMeta = { metaData ->
+    metaData.columnCount.times { i ->
         printf("%-20s", metaData.getColumnLabel(i + 1))
     }
     println()
@@ -38,5 +39,73 @@ rows = sql.rows('select * from weather')
 println("Weather info available for ${rows.size()} cities")
 println()
 
+bldr = new MarkupBuilder()
 
+bldr.weather {
+    sql.eachRow('select * from weather') {
+        city(name: it.city, temperature: it.temperature)
+    }
+}
+println()
+
+/**
+ * groovy 3.0.5 + mysql 8.0.22 报错如下
+ *
+ * DataSet unable to evaluate expression. AST not available for closure: com.moqi.ch09.A01UseDataBase$_run_closure5. Is the source code on the classpath?
+ */
+dataSet = sql.dataSet('weather')
+citiesBelowFreezing = dataSet.findAll {it.temperature < 32}
+println("Cities below freezing:")
+try {
+    citiesBelowFreezing.each {
+        println(it.city)
+    }
+} catch (GroovyRuntimeException e) {
+    println(e.message)
+}
+println()
+
+println("Number of cites: " + sql.rows('select * from weather').size())
+dataSet.add(city: 'Denver', temperature: 19)
+println("Number of cites: " + sql.rows('select * from weather').size())
+println()
+
+temperature = 50
+sql.executeInsert("""
+insert into weather values ('Oklahoma City', ${temperature})
+""")
+println(sql.firstRow("""
+select * from weather where city='Oklahoma City'
+"""))
+println()
+
+final String WEATHER_EXCEL = '/Users/moqi/Code/groovy-demo/src/main/resources/weather.xlsx'
+
+/*
+* 这里应该要外部配置 ODBC
+*
+* Caught: java.sql.SQLException: No suitable driver found for
+jdbc:odbc:Driver=
+{Microsoft Excel Driver (*.xls, *.xlsx, *.xlsm, *.xlsb)};
+DBQ=/Users/moqi/Code/groovy-demo/src/main/resources/weather.xlsx;READONLY=false
+
+java.sql.SQLException: No suitable driver found for
+jdbc:odbc:Driver=
+{Microsoft Excel Driver (*.xls, *.xlsx, *.xlsm, *.xlsb)};
+DBQ=/Users/moqi/Code/groovy-demo/src/main/resources/weather.xlsx;READONLY=false
+
+	at com.moqi.ch09.A01UseDataBase.run(A01UseDataBase.groovy:84)
+
+Process finished with exit code 1
+* */
+def excel_sql = Sql.newInstance("""
+jdbc:odbc:Driver=
+{Microsoft Excel Driver (*.xls, *.xlsx, *.xlsm, *.xlsb)};
+DBQ=${WEATHER_EXCEL};READONLY=false
+""", '', '')
+
+println("City\t\tTemperature")
+excel_sql.eachRow('select * from [temperatures$]') {
+    println("${it.city}\t\t${it.temperatures}")
+}
 
