8a19604dcdb59fb5428a71fcdb756561793b66d2
moqi <moqimoqidea@gmail.com>
2020-10-23 16:39:15 +0800

    使用分类注入方法

--- src/main/groovy/com/moqi/ch13/A01UsingCategories.groovy
+++ src/main/groovy/com/moqi/ch13/A01UsingCategories.groovy
@@ -24,3 +24,51 @@ try {
 } catch(MissingMethodException ex) {
   println ex.message
 }
+println()
+
+@Category(String)
+class A01StringUtilAnnotated {
+    def toSSN() {
+        if (size() == 9) {
+            "${this[0..2]}-${this[3..4]}-${this[5..8]}"
+        }
+    }
+}
+
+use(A01StringUtilAnnotated) {
+    println("123456789".toSSN())
+}
+println()
+
+class A01FindUtil {
+    def static extractOnly(String self, closure) {
+        def result = ''
+        self.each {
+            if (closure(it)) {result += it}
+        }
+        result
+    }
+}
+
+use(A01FindUtil) {
+    println("121254123".extractOnly{it == '4' || it == '5'})
+}
+println()
+
+use(A01StringUtil, A01FindUtil) {
+    str = "123487651"
+    println(str.toSSN())
+    println(str.extractOnly {it == '8' || it == '1'})
+}
+println()
+
+class A01Helper {
+    def static toString(String self) {
+        def method = self.metaClass.methods.find {it.name == 'toString'}
+        '!!' + method.invoke(self, null) + '!!'
+    }
+}
+
+use(A01Helper) {
+    println('hello world'.toString())
+}
