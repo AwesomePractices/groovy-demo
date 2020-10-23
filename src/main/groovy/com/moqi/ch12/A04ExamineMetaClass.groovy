4ca2ea4b75abc084737b11fbaac397671a65f48e
moqi <moqimoqidea@gmail.com>
2020-10-23 16:03:48 +0800

    动态行为关键类 ExamineMetaClass

---
+++ src/main/groovy/com/moqi/ch12/A04ExamineMetaClass.groovy
@@ -0,0 +1,9 @@
+package com.moqi.ch12
+
+/**
+ * 动态行为关键类 ExamineMetaClass
+ *
+ * @author moqi* On 10/23/20 15:32
+ */
+Integer.metaClass.invokeMethod = {String name, args -> /* */}
+println(Integer.metaClass.getClass().name)
