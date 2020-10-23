11b097f615f61ba9f80b8aab391022545ed1c2e7
moqi <moqimoqidea@gmail.com>
2020-10-23 17:17:09 +0800

    使用 ExpandoMetaClass 注入方法 Append Data

--- src/main/groovy/com/moqi/ch13/A02UsingExpandoMetaClass.groovy
+++ src/main/groovy/com/moqi/ch13/A02UsingExpandoMetaClass.groovy
@@ -19,3 +19,25 @@ Integer.metaClass.getDaysFromNow = { ->
     today.time
 }
 println 5.daysFromNow
+
+println()
+
+Integer.metaClass.'static'.isEven = {val -> val % 2 == 0}
+println("Is 2 even? " + Integer.isEven(2))
+println("Is 3 even? " + Integer.isEven(3))
+println()
+
+Integer.metaClass.constructor << {Calendar calendar ->
+    new Integer(calendar.get(Calendar.DAY_OF_YEAR))
+}
+println(new Integer(Calendar.instance))
+println()
+
+Integer.metaClass.constructor = {int val ->
+    println("Intercepting constructor call")
+    constructor = Integer.class.getConstructor(Integer.TYPE)
+    constructor.newInstance(val)
+}
+println(new Integer(4))
+println(new Integer(Calendar.instance))
+
