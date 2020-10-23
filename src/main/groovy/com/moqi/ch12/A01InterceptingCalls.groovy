d85923a26bd8e8a112a74ddb94c6123f172034e0
moqi <moqimoqidea@gmail.com>
2020-10-23 15:21:18 +0800

    使用 GroovyInterceptable 拦截方法

---
+++ src/main/groovy/com/moqi/ch12/A01InterceptingCalls.groovy
@@ -0,0 +1,44 @@
+package com.moqi.ch12
+
+/**
+ * 使用 GroovyInterceptable 拦截方法
+ *
+ * @author moqi* On 10/23/20 15:01
+ */
+class A01Car implements GroovyInterceptable {
+    def check() { System.out.println("check called...") }
+
+    def start() { System.out.println("start called...") }
+
+    def drive() { System.out.println("drive called...") }
+
+    def invokeMethod(String name, args) {
+        System.out.println("")
+        System.out.println("Call to $name intercepted... ")
+
+        if (name != 'check') {
+            System.out.println("running filter...")
+            A01Car.metaClass.getMetaMethod('check').invoke(this, null)
+        }
+
+        def validMethod = A01Car.metaClass.getMetaMethod(name, args)
+        if (validMethod != null) {
+            validMethod.invoke(this, args)
+        } else {
+            A01Car.metaClass.invokeMethod(this, name, args)
+        }
+        System.out.println("")
+    }
+}
+
+A01Car car = new A01Car()
+
+car.start()
+car.drive()
+car.check()
+
+try {
+    car.speed()
+} catch (Exception e) {
+    println(e)
+}
