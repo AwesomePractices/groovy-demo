aae8001aea50b0553a544c008005997bec3975db
moqi <moqimoqidea@gmail.com>
2020-10-26 17:11:42 +0800

    坏味道的代码

---
+++ src/main/groovy/com/moqi/ch16/A01Smelly.groovy
@@ -0,0 +1,14 @@
+package com.moqi.ch16
+
+/**
+ * 坏味道的代码
+ *
+ * @author moqi On 10/26/20 17:10
+ */
+def canVote(a) {
+    a > 17 ? "You can vote" : "You can't vote"
+}
+
+def p(instance) {
+    // 用于打印实例的代码
+}
