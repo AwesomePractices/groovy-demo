5607fd67b682bd173216e5107dfcea6ec37e77b7
moqi <moqimoqidea@gmail.com>
2020-10-26 17:37:28 +0800

    代码检查

--- src/main/groovy/com/moqi/ch16/A02CodeCheck.groovy
+++ src/main/groovy/com/moqi/ch16/A02CodeCheck.groovy
@@ -1,12 +1,16 @@
 package com.moqi.ch16
 
+import org.codehaus.groovy.ast.ASTNode
 import org.codehaus.groovy.ast.ClassNode
 import org.codehaus.groovy.ast.ConstructorNode
 import org.codehaus.groovy.ast.FieldNode
 import org.codehaus.groovy.ast.GroovyClassVisitor
 import org.codehaus.groovy.ast.MethodNode
 import org.codehaus.groovy.ast.PropertyNode
+import org.codehaus.groovy.control.CompilePhase
 import org.codehaus.groovy.control.SourceUnit
+import org.codehaus.groovy.transform.ASTTransformation
+import org.codehaus.groovy.transform.GroovyASTTransformation
 import sun.tools.jstat.SyntaxException
 
 /**
@@ -14,6 +18,18 @@ import sun.tools.jstat.SyntaxException
  *
  * @author moqi On 10/26/20 17:20
  */
+@GroovyASTTransformation(phase = CompilePhase.SEMANTIC_ANALYSIS)
+class A02CodeCheck implements ASTTransformation {
+
+    @Override
+    void visit(ASTNode[] nodes, SourceUnit sourceUnit) {
+        sourceUnit.ast.classes.each {classNode ->
+            classNode.visitContents(new A02OurClassVisitor(sourceUnit))
+        }
+    }
+}
+
+
 class A02OurClassVisitor implements GroovyClassVisitor {
 
     SourceUnit sourceUnit
@@ -35,7 +51,15 @@ class A02OurClassVisitor implements GroovyClassVisitor {
 
     @Override
     void visitMethod(MethodNode node) {
+        if(node.name.size() == 1) {
+            reportError("Make method name descriptive, avoid single letter names", node.lineNumber, node.columnNumber)
 
+            node.parameters.each {parameter ->
+                if (parameter.name.size() == 1) {
+                    reportError("Single letter parameters are morally wrong!", parameter.lineNumber, parameter.columnNumber)
+                }
+            }
+        }
     }
 
     @Override
@@ -48,4 +72,5 @@ class A02OurClassVisitor implements GroovyClassVisitor {
 
     }
 }
+
 
