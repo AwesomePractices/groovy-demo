d4cc431f80c31f39629037facc6015c620983042
moqi <moqimoqidea@gmail.com>
2020-10-27 10:24:19 +0800

    注入审计检测 injectMethodWithAudit 方法更新

--- src/main/groovy/com/moqi/ch16/A04InjectAudit.groovy
+++ src/main/groovy/com/moqi/ch16/A04InjectAudit.groovy
@@ -1,10 +1,9 @@
 package com.moqi.ch16
 
 import org.codehaus.groovy.ast.ASTNode
-import org.codehaus.groovy.ast.expr.ArgumentListExpression
-import org.codehaus.groovy.ast.expr.MethodCallExpression
-import org.codehaus.groovy.ast.expr.VariableExpression
+import org.codehaus.groovy.ast.builder.AstBuilder
 import org.codehaus.groovy.ast.stmt.ExpressionStatement
+import org.codehaus.groovy.ast.stmt.Statement
 import org.codehaus.groovy.control.CompilePhase
 import org.codehaus.groovy.control.SourceUnit
 import org.codehaus.groovy.transform.ASTTransformation
@@ -19,16 +18,17 @@ import org.codehaus.groovy.transform.GroovyASTTransformation
 class A04InjectAudit implements ASTTransformation {
     @Override
     void visit(ASTNode[] astNodes, SourceUnit sourceUnit) {
-        def checkingAccountClassNode = astNodes[0].classes.find {it.name == 'CheckingAccount'}
+        def checkingAccountClassNode = astNodes[0].classes.find { it.name == 'CheckingAccount' }
         injectAuditMethod(checkingAccountClassNode)
     }
 
     static void injectAuditMethod(checkingAccountClassNode) {
-        def nonAuditMethods = checkingAccountClassNode?.methods?.findAll {it.name != 'audit'}
-        nonAuditMethods?.each {injectMethodWithAudit(it)}
+        def nonAuditMethods = checkingAccountClassNode?.methods?.findAll { it.name != 'audit' }
+        nonAuditMethods?.each { injectMethodWithAudit(it) }
     }
 
-    static void injectMethodWithAudit(methodNode) {
+    // 第一个版本
+    /*static void injectMethodWithAudit(methodNode) {
         def callToAudit = new ExpressionStatement(
                 new MethodCallExpression(
                         new VariableExpression('this'),
@@ -38,5 +38,31 @@ class A04InjectAudit implements ASTTransformation {
         )
 
         methodNode.code.statements.add(0, callToAudit)
+    }*/
+
+    // 第二个版本
+    /*static void injectMethodWithAudit(methodNode) {
+        List<Statement> statements = new AstBuilder().buildFromSpec {
+            expression {
+                methodCall {
+                    variable 'this'
+                    constant 'audit'
+                    argumentList {
+                        methodNode.parameters.each { variable it.name }
+                    }
+                }
+            }
+        }
+
+        def callToCheck = statements[0]
+        methodNode.code.statements.add(0, callToCheck)
+    }*/
+
+    static void injectMethodWithAudit(methodNode) {
+        // def codeAsString = 'audit(amount)'
+        List<Statement> statements = new AstBuilder().buildFromCode { audit(amount) }
+
+        def callToAudit = statements[0].statements[0].expression
+        methodNode.code.statements.add(0, new ExpressionStatement(callToAudit))
     }
 }
