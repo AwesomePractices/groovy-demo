de76572d5675e92cf37a3dc95fb6698710870001
moqi <moqimoqidea@gmail.com>
2020-10-27 19:21:42 +0800

    使用 Groovy Mock Library 实现模拟 + 2

--- src/main/groovy/com/moqi/ch18/A14TestUsingMockFor.groovy
+++ src/main/groovy/com/moqi/ch18/A14TestUsingMockFor.groovy
@@ -1,18 +1,18 @@
 package com.moqi.ch18
 
-import groovy.mock.interceptor.StubFor
+import groovy.mock.interceptor.MockFor
 import groovy.test.GroovyTestCase
 
 /**
- * 使用 Groovy Mock Library 实现模拟
+ * 使用 Groovy Mock Library 实现模拟 + 1
  *
  * @author moqi On 10/27/20 19:09
  */
-class A13TestUsingStubFor extends GroovyTestCase {
+class A14TestUsingMockFor extends GroovyTestCase {
 
     void testMethodB() {
         def testObj = new A09ClassWithDependency()
-        def fileMock = new StubFor(FileWriter)
+        def fileMock = new MockFor(FileWriter)
         def text
 
         fileMock.demand.write { text = it.toString() }
--- src/main/groovy/com/moqi/ch18/A15TestMethodCUsingMock.groovy
+++ src/main/groovy/com/moqi/ch18/A15TestMethodCUsingMock.groovy
@@ -1,25 +1,25 @@
 package com.moqi.ch18
 
-import groovy.mock.interceptor.StubFor
+import groovy.mock.interceptor.MockFor
 import groovy.test.GroovyTestCase
 
 /**
- * 使用 Groovy Mock Library 实现模拟
+ * 使用 Groovy Mock Library 实现模拟 + 2
  *
  * @author moqi On 10/27/20 19:09
  */
-class A13TestUsingStubFor extends GroovyTestCase {
+class A15TestMethodCUsingMock extends GroovyTestCase {
 
-    void testMethodB() {
+    void testMethodC() {
         def testObj = new A09ClassWithDependency()
-        def fileMock = new StubFor(FileWriter)
+        def fileMock = new MockFor(FileWriter)
         def text
 
         fileMock.demand.write { text = it.toString() }
         fileMock.demand.close {}
 
         fileMock.use {
-            testObj.methodB(1)
+            testObj.methodC(1)
         }
 
         assertEquals("The value is 1.", text)
