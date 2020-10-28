24c6a08607db1b9c19370a4d851e3d1d5624aa1e
moqi <moqimoqidea@gmail.com>
2020-10-28 09:54:08 +0800

    DSL 上下文

---
+++ src/main/groovy/com/moqi/ch19/A02OrderPizza.groovy
@@ -0,0 +1,18 @@
+package com.moqi.ch19
+
+/**
+ * Order Pizza Groovy
+ *
+ * @author moqi On 10/28/20 09:51
+ */
+/*
+PizzaShop joesPizza = new PizzaShop()
+
+joesPizza.with {
+    setSize(Size.LARGE);
+    setCrust(Crust.THIN);
+    setTopping("Olives", "Onions", "Bell Pepper");
+    seetAddress("101 Main St., ...");
+    int time = setCart(CardType.VISA, "1234-1234-1234-1234");
+    printf("Pizza will arrive in %d minutes\n", time);
+}*/
---
+++ src/main/java/com/moqi/ch19/A01OrderPizza.java
@@ -0,0 +1,21 @@
+package com.moqi.ch19;
+
+/**
+ * Order Pizza
+ *
+ * @author moqi On 10/28/20 09:47
+ */
+
+public class A01OrderPizza {
+
+    public static void main(String[] args) {
+        /*PizzaShop joesPizza = new PizzaShop();
+        joesPizza.setSize(Size.LARGE);
+        joesPizza.setCrust(Crust.THIN);
+        joesPizza.setTopping("Olives", "Onions", "Bell Pepper");
+        joesPizza.seetAddress("101 Main St., ...");
+        int time = joesPizza.setCart(CardType.VISA, "1234-1234-1234-1234");
+        System.out.printf("Pizza will arrive in %d minutes\n", time);*/
+    }
+
+}
